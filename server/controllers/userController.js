40e7d6477476ca219f5d0aeb436f8e23a77b6966
fahmialis <fsinergia@yahoo.com>
2021-03-18 17:07:56 +0700

    add register dan login tapi masih kosong

---
+++ server/controllers/userController.js
@@ -0,0 +1,14 @@
+const { User } = require('../models')
+
+class UserController {
+    static register(req, res){
+
+    }
+
+    static login(req, res){
+        
+    }
+
+}
+
+module.exports = UserController
