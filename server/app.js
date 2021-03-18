469d29591ccfef5f37905da0fa2546a915154651
fahmialis <fsinergia@yahoo.com>
2021-03-18 16:55:54 +0700

    add socket.io listener

--- server/app.js
+++ server/app.js
@@ -5,12 +5,26 @@ const app = express()
 const router = require('./routers')
 const port = process.env.PORT || 3000
 const cors = require('cors')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+// listening
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+http.listen(3000, () => {
+    console.log(`listening on *:${port}`);
+});
 
 app.use(router)
 
