d8ac864d95a5f3c528a90d291fb130290f78f954
mufendew <m.arkanmufadho@gmail.com>
2021-03-19 14:33:58 +0700

    add procfile

--- server/app.js
+++ server/app.js
@@ -2,12 +2,10 @@
 
 const express = require('express')
 const app = express()
-const router = require('./routers')
 const port = process.env.PORT || 3000
 const cors = require('cors')
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const fs = require('fs');
 
 
 app.use(cors())
